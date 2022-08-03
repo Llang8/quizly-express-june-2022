270d03846565c7145816b26172d3a091c20cf93b
Lucas Lang <32493084+Llang8@users.noreply.github.com>
2022-08-03 12:09:00 -0500

    Added routes + templates folder

--- server.js
+++ server.js
@@ -9,9 +9,14 @@ const schema = require('./src/graphql/schema')
 
 const { connectDB } = require('./src/db')
 
+const initializeRoutes = require('./src/routes')
+
 const app = express()
 
 connectDB()
+
+app.set('view engine', 'ejs')
+app.set('views', './src/templates/views')
 
 app.use('/graphql', graphqlHTTP({
     schema,
@@ -21,6 +26,8 @@ app.use('/graphql', graphqlHTTP({
 app.get('/', (req,res) => {
     res.send('Hello world')
 })
+
+initializeRoutes(app)
 
 app.listen(process.env.PORT, (req, res) => {
     console.log(`Server running on PORT ${ process.env.PORT }`)
